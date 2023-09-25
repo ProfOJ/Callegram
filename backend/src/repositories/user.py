810291a94a3dd1b433f3c030e213ebedbc2b30f2
Arterialist <qazedcwsxrfv228@gmail.com>
2023-09-25 11:25:57 +0400

    chore(backend): load schedule when fetching user

--- backend/src/repositories/user.py
+++ backend/src/repositories/user.py
@@ -2,23 +2,23 @@ from sqlalchemy import Column
 from sqlalchemy.orm import joinedload, selectinload
 
 from database.models import User
-from .base import SQLAlchemyRepository
+from repositories.base import SQLAlchemyRepository
 
 
 class UsersRepository(SQLAlchemyRepository):
     model = User
 
     async def find_one(self, id: int, **kwargs):
-        return await super().find_one(id, options=joinedload(User.schedule))
+        return await super().find_one(id, options=[joinedload(User.schedule)])
 
     async def find_one_or_none(self, id: int, **kwargs):
-        return await super().find_one_or_none(id, options=joinedload(User.schedule))
+        return await super().find_one_or_none(id, options=[joinedload(User.schedule)])
 
     async def find_one_by(self, column: Column, value, **kwargs):
-        return await super().find_one_by(column, value, options=joinedload(User.schedule))
+        return await super().find_one_by(column, value, options=[joinedload(User.schedule)])
 
     async def find_one_or_none_by(self, column: Column, value, **kwargs):
-        return await super().find_one_or_none_by(column, value, options=joinedload(User.schedule))
+        return await super().find_one_or_none_by(column, value, options=[joinedload(User.schedule)])
 
     async def find_all(self, **kwargs):
         return await super().find_all(**kwargs)

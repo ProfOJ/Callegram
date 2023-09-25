76f32d0af7dd67e8c58bfeca46246df55641e6a1
Arterialist <qazedcwsxrfv228@gmail.com>
2023-09-25 11:24:05 +0400

    chore(backend): load user when fetching schedule

--- backend/src/repositories/schedule.py
+++ backend/src/repositories/schedule.py
@@ -8,10 +8,10 @@ class SchedulesRepository(SQLAlchemyRepository):
     model = Schedule
 
     async def find_one(self, id: str, **kwargs):
-        return await super().find_one(id, options=joinedload(Schedule.user))
+        return await super().find_one(id, options=[joinedload(Schedule.user)])
 
     async def find_one_or_none(self, id: str, **kwargs):
-        return await super().find_one_or_none(id, options=joinedload(Schedule.user))
+        return await super().find_one_or_none(id, options=[joinedload(Schedule.user)])
 
     async def find_one_by_user_id(self, user_id: int):
-        return await super().find_one_or_none_by(self.model.user_id, user_id, options=joinedload(Schedule.user))
+        return await super().find_one_or_none_by(self.model.user_id, user_id, options=[joinedload(Schedule.user)])

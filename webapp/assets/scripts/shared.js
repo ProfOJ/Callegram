6f8020338bb69f9d1ebe6dea53708fda6f1b2744
Arterialist <qazedcwsxrfv228@gmail.com>
2023-09-26 10:01:27 +0400

    chore(webapp): extract common headers

--- webapp/assets/scripts/shared.js
+++ webapp/assets/scripts/shared.js
@@ -35,12 +35,16 @@ function getInitData() {
   return initDataObject;
 }
 
+function getCommonHeaders() {
+    return {
+        Authorization: btoa(Telegram.WebApp.initData),
+        "Content-Type": "application/json",
+    };
+}
+
 async function authUser() {
   const response = await fetch("http://127.0.0.1:5000/user/auth", {
-    headers: {
-      Authorization: btoa(Telegram.WebApp.initData),
-      "Content-Type": "application/json",
-    },
+    headers: getCommonHeaders(),
     mode: "cors",
     method: "POST",
     body: JSON.stringify({

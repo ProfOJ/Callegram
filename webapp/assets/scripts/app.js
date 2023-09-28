7f8dbf41ebd05ad4a26f4a933dbf5d6f9f906e61
Arterialist <qazedcwsxrfv228@gmail.com>
2023-09-28 11:59:16 +0400

    feat(webapp): load busy days and update ui

--- webapp/assets/scripts/app.js
+++ webapp/assets/scripts/app.js
@@ -1,10 +1,31 @@
 async function onDayClicked(event) {
   const weekDayElement = event.target;
-  const date = new Date(weekDayElement.dataset.date);
+  const allDays = document.getElementsByClassName("weekDay");
+  for (const day of allDays) {
+    day.classList.remove("selected");
+  }
+  weekDayElement.classList.add("selected");
+
+  const date = new Date(weekDayElement.getAttribute("data-date"));
+  date.setUTCHours(0, 0, 0, 0);
 
   const events = await getEventsForDate(date);
   if (!events) {
     return;
+  }
+}
+
+function disableFreeDays(busyDays) {
+  // busyDays example: ["2023-09-28", "2023-09-29", "2023-10-02"]
+  const weekDayElements = document.getElementsByClassName("weekDay");
+
+  for (const weekDayElement of weekDayElements) {
+    const date = new Date(weekDayElement.getAttribute("data-date"));
+
+    const dateString = date.toISOString().split("T")[0];
+    if (!busyDays.includes(dateString)) {
+      weekDayElement.classList.add("unavailable");
+    }
   }
 }
 
@@ -16,10 +37,25 @@ async function main() {
 
   populateDays();
 
+  const today = new Date();
+  const dateEnd = new Date(
+    Date.UTC(today.getFullYear(), today.getMonth(), today.getDate() + 15)
+  );
+  today.setUTCHours(0, 0, 0, 0);
+  const busyDays = await getBusyDays(today, dateEnd);
+
+  disableFreeDays(busyDays);
+
   const weekDayElements = document.getElementsByClassName("weekDay");
+  let checkedFirstBusyDay = false;
   for (const weekDayElement of weekDayElements) {
     if (weekDayElement.classList.contains("unavailable")) {
       continue;
+    }
+
+    if (!checkedFirstBusyDay) {
+      weekDayElement.classList.add("selected");
+      checkedFirstBusyDay = true;
     }
 
     weekDayElement.addEventListener("click", (event) => {
@@ -27,12 +63,15 @@ async function main() {
     });
   }
 
-  // get current date in utc with local day
-  const today = new Date();
-  const date = new Date(
-    Date.UTC(today.getFullYear(), today.getMonth(), today.getDate())
+  const firstBusyDate = new Date(busyDays[0]);
+  const firstBusyDateUTC = new Date(
+    Date.UTC(
+      firstBusyDate.getFullYear(),
+      firstBusyDate.getMonth(),
+      firstBusyDate.getDate()
+    )
   );
-  const events = await getEventsForDate(date);
+  const events = await getEventsForDate(firstBusyDateUTC);
 
   showSection(1);
 }
--- webapp/assets/scripts/network.js
+++ webapp/assets/scripts/network.js
@@ -6,7 +6,7 @@ async function getEventsForDate(date) {
       mode: "cors",
     }
   );
-  
+
   if (response.status === 401) {
     Telegram.WebApp.showAlert("Unauthenticated");
     return;
@@ -19,4 +19,28 @@ async function getEventsForDate(date) {
   }
 
   return responseData.data.events;
+}
+
+
+async function getBusyDays(from, to) {
+    const response = await fetch(
+        `http://localhost:5000/schedule/busy_days?from_date=${from.toISOString()}&to_date=${to.toISOString()}`,
+        {
+        headers: getCommonHeaders(),
+        mode: "cors",
+        }
+    );
+    
+    if (response.status === 401) {
+        Telegram.WebApp.showAlert("Unauthenticated");
+        return;
+    }
+    
+    const responseData = await response.json();
+    if (!responseData.success) {
+        Telegram.WebApp.showAlert(data.message);
+        return;
+    }
+    
+    return responseData.data.busy_days;
 }

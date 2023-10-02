5a7701d722bbcb84033a1737d619a6cd14934aef
Arterialist <qazedcwsxrfv228@gmail.com>
2023-10-02 15:30:26 +0400

    fix(backend): add missing import

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from bot.bot import get_bot_instance
 from config import BOT_TOKEN
 from scheduler.scheduler import scheduler
 from services.auth import AuthService
@@ -14,7 +15,7 @@ UOWDep = Annotated[AbstractUnitOfWork, Depends(UnitOfWork)]
 
 
 def get_notification_service():
-    return TelegramNotificationService(bot)
+    return TelegramNotificationService(get_bot_instance())
 
 
 def get_scheduler():
